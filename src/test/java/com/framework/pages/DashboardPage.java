b5f053594e2b536521fa8317fdb75176780b7dd6
Vikram Jangid <Vikram.Jangid@dotmatics.com>
2025-08-05 22:23:45 +0530

    Add CountryLanguageDialog and update DashboardPage
    Introduced CountryLanguageDialog page object to handle country and language selection. Updated DashboardPage to automatically switch to the India website if not already selected, utilizing the new dialog. Also added Allure step for better reporting.

--- src/test/java/com/framework/pages/DashboardPage.java
+++ src/test/java/com/framework/pages/DashboardPage.java
@@ -1,6 +1,7 @@
 package com.framework.pages;
 
 import com.framework.base.BasePage;
+import io.qameta.allure.Allure;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
@@ -11,7 +12,24 @@ public class DashboardPage extends BasePage {
     public DashboardPage(WebDriver driver) {
         super(driver);
         clickElement(By.xpath("//span[@data-cy='closeModal']"), "Cross Button of 'Login/Signup With' dialog");
+        this.changeToIndiaWebsite();
         waitUntilVisible(By.xpath(navigationBarXpath), "Dashboard navigation menu bar");
+    }
+
+    public void changeToIndiaWebsite() {
+        String countryLanguageSelectionXpath = "//span[@data-testid=\"country-lang-switcher\"]";
+        String text = this.getElementText(By.xpath(countryLanguageSelectionXpath), "country-lang-switcher");
+        if (text.contains("INR")) {
+            return;
+        }
+        else {
+            Allure.step("Change country and language to INR", () -> {
+                this.clickElement(By.xpath(countryLanguageSelectionXpath), "country-lang-switcher");
+                CountryLanguageDialog countryLanguageDialog = new CountryLanguageDialog(this.driver);
+                countryLanguageDialog.changeCountryAndApply();
+                clickElement(By.xpath("//span[@data-cy='closeModal']"), "Cross Button of 'Login/Signup With' dialog");
+            });
+        }
     }
 
     public void navigateTo(String menuItemName) {
